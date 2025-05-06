a5a467fcdf06c7b04f2145b7eeea131f12b927cc
Dilmi Navodya Dehigolla <navodyadehigolla@gmail.com>
2025-05-06 15:16:44 +0530

    appinventory

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import './App.css';
 import Header from "./component/Header";
 import './index.css';
-import Checkout from "./pages/Checkout";
 import AdminEvents from "./pages/AdminEvents";
 import AdminAddEvent from './pages/AdminAddEvent';
+import AboutUs  from './pages/AboutUs';
+import SignUpPage from "./pages/SignUp"; // Import SignUpPage
+import Checkout from "./pages/Checkout"; // Import Checkout
+import HomePage from "./pages/HomePage"; // Import Home Page
 import Footer from "./component/Footer";
 import AdminProduct from "./pages/AdminProduct";
 import CustomerViewEvent from "./pages/CustomerViewEvent";
+<<<<<<< HEAD
 import CustomerProduct from "./pages/CustomerProduct";
+=======
+import Cart from "./pages/Cart"; // Import Cart	
+import Invoice from "./pages/Invoice"; // Import Invoice
+>>>>>>> 507bf2e885906d4b87c3058650f6da8309c69993
 
 function App() {
   return (
@@ -26,8 +35,9 @@ function AppWithRoutes() {
       {location.pathname !== '/AdminEvents' && location.pathname !== '/AdminAddEvent' && location.pathname !== '/AdminProduct' && location.pathname !== '/AdminProduct'&& <Header />}
       
 
+      {/* Routing between pages */}
       <Routes>
-        <Route path="/" element={<Checkout />} />
+        {/* Default route - Checkout */}
 
         <Route path="/adminevents" element={<AdminEvents />} />
 
@@ -38,12 +48,28 @@ function AppWithRoutes() {
         <Route path="/customerviewevent" element={<CustomerViewEvent />} />
         <Route path="/customerproduct" element={<CustomerProduct />} />
 
+        {/* Cart route */}
+        <Route path="/cart" element={<Cart />} />
+
+        <Route path="/invoice" element={<Invoice />} />
+          <Route path="/" element={<AboutUs />} />
+            <Route path="/checkout" element={<AboutUs />} />
+               {/* Home Page route */}
+        <Route path="/home" element={<HomePage />} />
+
+        {/* Sign Up page route */}
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
 
+<<<<<<< HEAD
       {location.pathname !== '/AdminEvents' && location.pathname !== '/AdminAddEvent' &&  location.pathname !== '/AdminProduct' && <Footer />}
+=======
+      {location.pathname !== '/AdminEvents' && location.pathname !== '/AdminAddEvent' && <Footer />}
+>>>>>>> 507bf2e885906d4b87c3058650f6da8309c69993
       
     </>
   );
 }
+
 
 export default App;
